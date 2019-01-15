9e4662dba0c582391c18a3376aba67daad3132ae
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-01-15 13:41:14 +0300

    was changed test cases to Parameterized Test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,60 +1,42 @@
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
+@RunWith(value = Parameterized.class)
 public class SimpleTest {
     private ArrayList<Integer> a;
     private ArrayList<Integer> b;
+    private String expectedA;
+    private String expectedB;
 
-    @Test
-    public void merge1() {
-        a = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-        b = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-        Simple.merge(a, b);
-        assertEquals(a.toString(), "[1, 1, 2, 2, 3, 3, 4, 4, 5, 5]");
+    public SimpleTest(ArrayList<Integer> a, ArrayList<Integer> b, String sA) {
+        this.expectedA = sA;
+        this.expectedB = b.toString();
+        this.a = a;
+        this.b = b;
+    }
+
+    @Parameterized.Parameters
+    public static Iterable<Object[]> dataForTest() {
+        return Arrays.asList(new Object[][]{
+                {new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5)), new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5)), "[1, 1, 2, 2, 3, 3, 4, 4, 5, 5]"},
+                {new ArrayList<Integer>(Arrays.asList(-100, 0, 3, 8, 44, 59)), new ArrayList<Integer>(Arrays.asList(-5, -1)), "[-100, -5, -1, 0, 3, 8, 44, 59]"},
+                {new ArrayList<Integer>(Arrays.asList(1)), new ArrayList<Integer>(Arrays.asList(0, 3, 4, 5)), "[0, 1, 3, 4, 5]"},
+                {new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5)), new ArrayList<Integer>(Arrays.asList(-58)), "[-58, 1, 2, 3, 4, 5]"},
+                {new ArrayList<Integer>(Arrays.asList(2, 3, 8, 44, 59)), new ArrayList<Integer>(Arrays.asList(-5, -1)), "[-5, -1, 2, 3, 8, 44, 59]"},
+                {new ArrayList<Integer>(Arrays.asList(6, 8, 9, 15, 88)), new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5)), "[1, 2, 3, 4, 5, 6, 8, 9, 15, 88]"}
+        });
     }
 
     @Test
-    public void merge2() {
-        a = new ArrayList<Integer>(Arrays.asList(-100, 0, 3, 8, 44, 59));
-        b = new ArrayList<Integer>(Arrays.asList(-5, -1));
+    public void merge() {
         Simple.merge(a, b);
-        assertEquals(a.toString(), "[-100, -5, -1, 0, 3, 8, 44, 59]");
+        assertEquals(a.toString(), expectedA);
+        assertEquals(b.toString(), expectedB);
     }
-
-    @Test
-    public void merge3() {
-        a = new ArrayList<Integer>(Arrays.asList(6, 8, 9, 15, 88));
-        b = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-        Simple.merge(a, b);
-        assertEquals(a.toString(), "[1, 2, 3, 4, 5, 6, 8, 9, 15, 88]");
-    }
-
-    @Test
-    public void merge4() {
-        a = new ArrayList<Integer>(Arrays.asList(1));
-        b = new ArrayList<Integer>(Arrays.asList(0, 3, 4, 5));
-        Simple.merge(a, b);
-        assertEquals(a.toString(), "[0, 1, 3, 4, 5]");
-    }
-
-    @Test
-    public void merge5() {
-        a = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-        b = new ArrayList<Integer>(Arrays.asList(-58));
-        Simple.merge(a, b);
-        assertEquals(a.toString(), "[-58, 1, 2, 3, 4, 5]");
-    }
-
-    @Test
-    public void merge6() {
-        a = new ArrayList<Integer>(Arrays.asList(2, 3, 8, 44, 59));
-        b = new ArrayList<Integer>(Arrays.asList(-5, -1));
-        Simple.merge(a, b);
-        assertEquals(a.toString(), "[-5, -1, 2, 3, 8, 44, 59]");
-    }
-
 }
