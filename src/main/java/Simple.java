a00834e2bf6e8b5a5d6bae063736cef6ee3f9293
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-01-15 13:13:01 +0300

    my first solution

--- src/main/java/Simple.java
+++ src/main/java/Simple.java
@@ -2,6 +2,16 @@ import java.util.ArrayList;
 
 public class Simple {
     static void merge(ArrayList<Integer> a, ArrayList<Integer> b) {
+        final int lenB = b.size();
 
+        for (int indexB = 0,indexA = 0; indexB < lenB; indexA++) {
+            if (indexA < a.size()) {
+                if (a.get(indexA) > b.get(indexB)) {
+                    a.add(indexA, b.get(indexB++));
+                }
+            } else {
+                a.add(b.get(indexB++));
+            }
+        }
     }
 }
