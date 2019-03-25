04a31a1eaaef96324c951f76bf459885bf50ce75
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-03-25 15:25:46 +0300

    last changes in TaskOne

--- TaskOne/src/main/java/MainRunner.java
+++ TaskOne/src/main/java/MainRunner.java
@@ -4,8 +4,6 @@ import data.NumberUsingWords;
 import thread.Adder;
 import thread.CleanerMin;
 
-import static java.lang.Thread.sleep;
-
 public class MainRunner {
     public static void main(String[] args) {
         CustomArray<NumberUsingWords> data = new CustomArrayImpl<>();
--- TaskOne/src/main/java/data/CustomArrayImpl.java
+++ TaskOne/src/main/java/data/CustomArrayImpl.java
@@ -3,23 +3,15 @@ package data;
 import java.util.ArrayList;
 
 public class CustomArrayImpl<E extends Comparable<E>> implements CustomArray<E> {
-    private ArrayList<Integer> freeIndex;
     private ArrayList<E> data;
 
     public CustomArrayImpl() {
         this.data = new ArrayList<>();
-        this.freeIndex = new ArrayList<>();
     }
 
     public void add(E item) {
         synchronized (data) {
-            if (freeIndex.isEmpty()) {
-                data.add(item);
-            } else {
-                int ind = freeIndex.get(freeIndex.size() - 1);
-                freeIndex.remove(freeIndex.size() - 1);
-                data.set(ind, item);
-            }
+            data.add(item);
         }
     }
 
@@ -30,14 +22,15 @@ public class CustomArrayImpl<E extends Comparable<E>> implements CustomArray<E> 
 
             for (int i = 0, len = data.size(); i < len; i++) {
                 E temp = data.get(i);
-                if (temp != null && (min == null || min.compareTo(temp) > 0)) {
+                if (min == null || temp.compareTo(min) < 0) {
                     min = temp;
                     iMin = i;
                 }
             }
 
             if (min != null)
-                data.set(iMin, null);
+                data.remove(iMin);
+
             return min;
         }
     }
