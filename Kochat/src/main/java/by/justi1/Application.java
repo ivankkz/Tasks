2736a10f99cba9d9162439f35396245c85201c47
Ivan Yakimovich <ivan.yakimovich1@gmail.com>
2019-04-01 18:03:22 +0300

    #Kochat init

---
+++ Kochat/src/main/java/by/justi1/Application.java
@@ -0,0 +1,14 @@
+package by.justi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+https://o7planning.org/ru/10719/create-a-simple-chat-application-with-spring-boot-and-websocket
+*/
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
